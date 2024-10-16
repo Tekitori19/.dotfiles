d89a2f5267ad5b0d34ea3a785af9015cddd6318c
Dwcks <corclan19@gmail.com>
2024-10-16 08:12:51 +0700

    added tmux like itergrator, copilot and new color scheme in neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,6 +1,6 @@
 -- bootstrap lazy.nvim, LazyVim and your plugins
 require("config.lazy")
-vim.opt.scrolloff = 8
+vim.opt.scrolloff = 10
 -- vim.api.nvim_set_option("clipboard", "unnamed")
 -- Check if 'pwsh' is executable and set the shell accordingly
 if vim.fn.executable("pwsh") == 1 then
@@ -44,7 +44,7 @@ if vim.g.neovide then
 
   vim.g.neovide_scroll_animation_length = 0
 
-  vim.g.neovide_transparency = 0.86
+  -- vim.g.neovide_transparency = 0.86
 
   vim.g.neovide_padding_top = 0
   vim.g.neovide_padding_left = 0
